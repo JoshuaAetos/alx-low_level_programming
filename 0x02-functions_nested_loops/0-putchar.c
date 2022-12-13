e2771a400f62227db1024625212f7a071893e71f
JoshuaAetos <egbunujoshua1@yahoo.com>
2022-12-13 05:58:55 -0800

    fixing putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,17 @@
 #include "main.h"
-
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
+ */
 int main(void)
 {
-	_putchar('_');
-	_putchar('P');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('c');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char c[] = "Holberton";
+	int i;
 
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
