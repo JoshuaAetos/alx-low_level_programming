ce79b1d3e35066afc0d8c1b81fe295147246b1dc
JoshuaAetos <egbunujoshua1@yahoo.com>
2022-12-19 10:42:11 -0800

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_array - Entry point
- * Desc: Entry
- *@a: pointer
- *@n: var
- * Return: Always 0 (Success)
+ *print_array - prints an array of size n
+ *
+ *@a: pointer that will store the array's location
+ *@n: size of the array
+ *
+ *Return: nothing
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", *(a + i));
-		if (i < (n - 1))
+		printf("%d", a[i]);
+		if (i < n - 1)
 		{
 			printf(", ");
 		}
