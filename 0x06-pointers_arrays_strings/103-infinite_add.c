e84238d9e01f2cfe76bcde8cfac6811384b587a2
JoshuaAetos <egbunujoshua1@yahoo.com>
2022-12-21 15:06:24 -0800

    0x05-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,103 +1,49 @@
-#include "main.h"                                                                                                                      
 
-#include <stdio.h>                                                                                                                      
+#include <stdio.h>                                                                                                                    
 
-                                                                                                                                       
+                                                                                                                                     
 
-/**                                                                                                                                    
+int main(void)                                                                                                                        
 
- * print_buffer - Prints a buffer 10 bytes at a time, starting with                                                                    
+{                                                                                                                                    
 
- *                the byte position, then showing the hex content,                                                                      
+        int n;                                                                                                                        
 
- *                then displaying printable charcaters.                                                                                
+        int a[5];                                                                                                                    
 
- * @b: The buffer to be printed.                                                                                                        
+        int *p;                                                                                                                      
 
- * @size: The number of bytes to be printed from the buffer.                                                                            
+                                                                                                                                     
 
- */                                                                                                                                    
+        a[2] = 1024;                                                                                                                  
 
-void print_buffer(char *b, int size)                                                                                                    
+        p = &n;                                                                                                                      
 
-{                                                                                                                                      
+/*                                                                                                                                    
 
-        int byte, index;
+ * write your line of code here...                                                                                                    
 
-       
+ * Remember:                                                                                                                          
 
-        for (byte = 0; byte < size; byte += 10)                                                                                        
+ * main - magic                                                                                                                      
 
-        {                                                                                                                              
+ * - you are not allowed to use a                                                                                                    
 
-                printf("%08x: ", byte);                                                                                                
+ *   - you are not allowed to modify p                                                                                                
 
-                                                                                                                                       
+ *   - only one statement                                                                                                            
 
-                for (index = 0; index < 10; index++)                                                                                    
+ *   - you are not allowed to code anything else than this line of code                                                              
 
-                {                                                                                                                      
+ */                                                                                                                                  
 
-                        if ((index + byte) >= size)                                                                                    
+        *(p + 5) = 98;                                                                                                                
 
-                                printf("  ");                                                                                          
+/* ...so that this prints 98\n */                                                                                                    
 
-                                                                                                                                       
+        printf("a[2] = %d\n", a[2]);                                                                                                  
 
-                        else                                                                                                            
-
-                                printf("%02x", *(b + index + byte));                                                                    
-
-                                                                                                                                       
-
-                        if ((index % 2) != 0 && index != 0)                                                                            
-
-                                printf(" ");                                                                                            
-
-                }                                                                                                                      
-
-                                                                                                                                       
-
-                for (index = 0; index < 10; index++)                                                                                    
-
-                {                                                                                                                      
-
-                        if ((index + byte) >= size)                                                                                    
-
-                                break;                                                                                                  
-
-                                                                                                                                       
-
-                        else if (*(b + index + byte) >= 31 &&                                                                          
-
-                                 *(b + index + byte) <= 126)                                                                            
-
-                                printf("%c", *(b + index + byte));                                                                      
-
-                                                                                                                                       
-
-                        else                                                                                                            
-
-                                printf(".");                                                                                            
-
-                }                                                                                                                      
-
-                                                                                                                                       
-
-                if (byte >= size)                                                                                                      
-
-                        continue;                                                                                                      
-
-                                                                                                                                       
-
-                printf("\n");                                                                                                          
-
-        }                                                                                                                              
-
-                                                                                                                                       
-
-        if (size <= 0)                                                                                                                  
-
-                printf("\n");                                                                                                          
+        return (0);                                                                                                                  
 
 }
+
