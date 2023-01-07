6d518ae89d4d4ca84ccaecae87612bfd0a61864f
JoshuaAetos <egbunujoshua1@yahoo.com>
2023-01-06 16:47:16 -0800

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ * @argc: The argument counter
+ * @argv: The argument vector
+ * Return: Always 0
+*/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ * @argc: The argument counter
+ * @argv: The argument vector
+ * Return: Return 0 always
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
